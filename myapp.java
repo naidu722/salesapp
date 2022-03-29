46ddaee83433f3acd1efd4a2bbc9f2c239ceb85d
naidu722 <102045760+naidu722@users.noreply.github.com>
2022-03-29 15:46:07 +0530

    just minor change
    added one line

--- myapp.java
+++ myapp.java
@@ -3,5 +3,6 @@ class myapp{
    public static void mian(string[] args){
      system.out.println("Hello World");
      system.out.println("thank you");
+     system.out.printing("adding more")
      }
     }
